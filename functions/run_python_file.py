2e0fa24bbba1b798997be32bc74e01176b729e62
EimanDev <e.babeckas@protonmail.com>
2026-07-09 23:53:55 +0200

    Finished up until CH3 L1

---
+++ functions/run_python_file.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+
+
+def run_python_file(
+    working_directory: str, file_path: str, args: list[str] | None = None
+) -> str:
+    try:
+        output = ""
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(
+            os.path.join(working_dir_abs, file_path))
+        valid_target_dir = os.path.commonpath(
+            [working_dir_abs, target_dir]) == working_dir_abs
+        if not valid_target_dir:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target_dir):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        if not file_path.endswith('.py'):
+            return f'Error: "{file_path}" is not a Python file'
+        else:
+            command = ["python", target_dir]
+            if args is not None:
+                command.extend(args)
+            result = subprocess.run(
+                command, capture_output=True, text=True, timeout=30, cwd=working_dir_abs)
+            if result.returncode != 0:
+                output += f"Process exited with code {result.returncode}"
+            if not result.stdout and not result.stderr:
+                output += "No output produced"
+            if result.stdout:
+                output += f"STDOUT:{result.stdout}"
+            if result.stderr:
+                output += f"STDERR:{result.stderr}"
+        return output
+
+    except Exception as e:
+        return f"Error: {e}"
